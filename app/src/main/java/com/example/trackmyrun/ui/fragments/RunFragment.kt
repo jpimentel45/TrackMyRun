44f88e29b5d526c11c8e5faf30faf48003d53148
Juan Pimentel <perra@juans-air.lan>
2021-04-25 15:49:51 -0700

    Adding Navigation for setup and run fragment

--- app/src/main/java/com/example/trackmyrun/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/trackmyrun/ui/fragments/RunFragment.kt
@@ -1,12 +1,22 @@
 package com.example.trackmyrun.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.trackmyrun.R
 import com.example.trackmyrun.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
     private val viewModel : MainViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
