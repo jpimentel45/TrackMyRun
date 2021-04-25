fd5d2cd09f6715b72cdfecc006ceb0c3a67c7e92
Juan Pimentel <perra@juans-air.lan>
2021-04-25 16:43:00 -0700

    Adding service for Tracking Fragment

--- app/src/main/java/com/example/trackmyrun/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/trackmyrun/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,13 @@
 package com.example.trackmyrun.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.trackmyrun.R
+import com.example.trackmyrun.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.trackmyrun.services.TrackingService
 import com.example.trackmyrun.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +21,9 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
         //Load the map
         mapView.apply {
             onCreate(savedInstanceState)
@@ -56,6 +62,12 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onSaveInstanceState(outState)
         mapView?.onSaveInstanceState(outState)
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also { intent ->
+            intent.action = action
+            requireContext().startService(intent)
+        }
 //    override fun onDestroy() {
 //        super.onDestroy()
 //        mapView?.onDestroy()
