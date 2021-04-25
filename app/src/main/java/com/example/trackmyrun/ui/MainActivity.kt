7725c74ee851479fca06336da39f19a246148127
Juan Pimentel <perra@juans-air.lan>
2021-04-25 15:44:17 -0700

    Adding bottom bar and setting fragments for navigation graph, set up bar on Main Activity

--- app/src/main/java/com/example/trackmyrun/ui/MainActivity.kt
+++ app/src/main/java/com/example/trackmyrun/ui/MainActivity.kt
@@ -3,21 +3,31 @@ package com.example.trackmyrun.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.trackmyrun.R
 import com.example.trackmyrun.db.RunDao
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO : RunDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}")
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { controller, destination, arguments ->
+                bottomNavigationView.visibility = when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment -> View.VISIBLE
+                    else -> View.VISIBLE
+                }
+            }
     }
 }
