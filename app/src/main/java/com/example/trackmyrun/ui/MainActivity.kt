18f8579a2e20854f6f0cb53e529021698d1fddec
Juan Pimentel <perra@juans-air.lan>
2021-04-25 16:16:57 -0700

    Adding Easy permissions for Run Fragment

--- app/src/main/java/com/example/trackmyrun/ui/MainActivity.kt
+++ app/src/main/java/com/example/trackmyrun/ui/MainActivity.kt
@@ -19,14 +19,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
         navHostFragment.findNavController()
             .addOnDestinationChangedListener { controller, destination, arguments ->
                 bottomNavigationView.visibility = when (destination.id) {
                     R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment -> View.VISIBLE
-                    else -> View.VISIBLE
+                    else -> View.INVISIBLE
                 }
             }
     }
